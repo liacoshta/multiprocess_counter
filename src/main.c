566d24225e882a4e113bc7749a1ad69b7dc8cee2
liacoshta <liacoshta@gmail.com>
2020-08-12 23:34:41 -0300

    só o 2 que n funciona

--- src/main.c
+++ src/main.c
@@ -18,83 +18,79 @@
 
 int a=0;
 
-       int Verificaprimo(int num){
+int Verificaprimo(int num){
 /*função que verifica cada primo*/
 	for (int i=2; i<num; i++){
-		if (num % i == 0|| num ==1){
- 			       
-         		a=0;//flag para alterar variável contador
+		if (num % i == 0||num==1){
+    	        	a=0;//não é primo
 			return a;
                         }
-	               }
-	    	if(num>=2)a=1;
-			return a;  			
+	           }
+	    	if(num>=2){
+			a=1;//é primo
+			}
+		return a;  			
 	}
 
-
-
-
-
 int main() {
-
-
-
-	int j=0;
-	int var[100];//vetor de variáveis que armazena número a ser avaliado
-
 	
   /* Definir flags de protecao e visibilidade de memoria */
   int protection = PROT_READ | PROT_WRITE;
   int visibility = MAP_SHARED | MAP_ANON;
 
   /* Criar area de memoria compartilhada */
+	int var[100];
 	int *nprocessos; 
 	int *contador;
 	int *i;
-  contador = (int*) mmap(NULL, sizeof(int)*100, protection, visibility, 0, 0);
-		
-	 if ((long int)contador==-1) printf("Erro de alocacao!\n");
-	i= (int*)mmap(NULL, sizeof(int)*100, protection, visibility, 0,0);
-	nprocessos = (int*)mmap(NULL, sizeof(int)*100, protection, visibility,0,0);
+contador = (int*) mmap(NULL, sizeof(int)*1, protection, visibility, 0, 0);
+if ((long int)contador==-1) printf("Erro de alocacao!\n");
+i = (int*)mmap(NULL, sizeof(int)*1, protection, visibility, 0,0);
+nprocessos = (int*)mmap(NULL, sizeof(int)*100, protection, visibility,0,0);
 	(*contador)=0;
 	(*i)=0;
 	(*nprocessos)=0;
-	//cria proecssos filhos
-	pid_t filho[3];
-	filho[0] = fork();/*
-	for (int j=0; j<3;j++){
-		filho[j] = fork();
-		(*nprocessos)++;
-	}*/
-
-		//esta parte só executa no processo filho
-		if (filho[(0)]==0){//execução do filho	
-
-			Verificaprimo(var[(*i)]);
-			if(Verificaprimo(var[(*i)])==1) (*contador)++;
-//			(*i)++;
-			exit(1);
 	
-		       } 
-	//esta parte executa no processo pai*/
-			char lixo;
+		char lixo;
+		int tam;
+		int j;
+ //leitura da entrada e armazenamento no vetor de variáveis
 			do{
-			scanf("%d",&var[(*i)]);
-			scanf("%c",&lixo);
-			 Verificaprimo(var[(*i)]);
-			if (Verificaprimo(var[(*i)])==1) (*contador)++;
-			if (lixo =='\n') break;
-			(*i)++;
-			}while (lixo !='\n');
-	//esta parte elimina os processos que ainda não acabaram*/
-		//	while ((*nprocessos)!=0){
-			waitpid(filho[(0)],0,0);
-		//	(*nprocessos)--;
-			//	}
-	/*esse é o fim do programa*/
-	printf("%d\n", *contador);	 
+                        scanf("%d",&(var[(*i)]));
+                        scanf("%c",&lixo);
+                        (*i)++;
+                        }while (lixo !='\n');
+//for (int j=0;j<(*i);j++) printf("%d\t",var[j]);	
+//printf("li todos os nu\n");
+	//cria proecssos filhos
+		pid_t filho[3];
+	
+	for (j=0; j<4;j++){
+		filho[j] = fork();
+	//esta parte só executa no processo filho
+		if (filho[j]==0){
+			while((*i)>=0){
+				int c, b;
+				c = (var[(*i)]);
+				(*i)--;
+			//	b=Verificaprimo(c);
+				if(Verificaprimo(c)){ 
+					(*contador)++;
+					}
+				}
+			exit(1);
+printf("sou o contador após if%d\n",(*contador));
+		
+				}
+//printf("sou o contador apos while%d\n", *contador);
+//exit(1);
+	  	}
+	 			
+		//printf("sou o contador antes waitpid %d\n",*contador);
+		/*esse é o fim do programa*/
+			for (int k=0;k<4;k++){
+				waitpid(filho[k],NULL,0);
+			}
+	printf("%d\n", (*contador));	 
 	exit(1);
-	return 0;
-
-
 }
